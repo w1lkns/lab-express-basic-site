9b9ddaf4c169028a7e5adc32b7dabcd13f36edf2
Wilkins Morales <wilkinscom@gmail.com>
2023-02-05 23:06:56 +0200

    add navbar links to html files

--- app.js
+++ app.js
@@ -12,25 +12,25 @@ app.use(express.static("public"));
 
 // 1st page /home
 app.get("/", (req, res) => {
-  console.log(request);
+  console.log(req);
   res.sendFile(__dirname + "/views/home-page.html");
 });
 
 // 2nd page /about
 app.get("/about", (req, res) => {
-  console.log(request);
+  console.log(req);
   res.sendFile(__dirname + "/views/about.html");
 });
 
 // 3rd page /works
 app.get("/works", (req, res) => {
-  console.log(request);
+  console.log(req);
   res.sendFile(__dirname + "/views/works.html");
 });
 
 // 4th page /gallery
 app.get("/gallery", (req, res) => {
-    console.log(request);
+    console.log(req);
     res.sendFile(__dirname + "/views/gallery.html");
   });
 
